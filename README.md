eb276372dd5e27415ce25e5e051993365d571bc0
Satoshi Nagashiba <bobtabo.buhibuhi@gmail.com>
2021-02-23 23:09:19 +0900

    新規作成

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# docker5
+この記事用です。
+
+https://qiita.com/_bobtabo_/items/e76a14bd7ed65b5e34b1
+
+
+# インストールするもの
+* PHP（PHP-FPM）8.x
+* Nginx 最新版
+* MySQL 8.x
+* Composer 2.x
+* Laravel 8.x
+* Redis 最新版
+* MailHog 最新版
+* XDebug 3.x
---
+++ src/laravel/bin/clear-laravel.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+php artisan view:clear
+php artisan cache:clear
+php artisan config:clear
+php artisan route:clear
+php artisan clear-compiled
+php artisan config:cache
+composer dump-autoload
+php artisan ide-helper:generate
+php artisan ide-helper:models -N
+php artisan ide-helper:meta
+find . -name '._.DS_Store' -type f -ls -delete
